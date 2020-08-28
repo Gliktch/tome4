43bfa8394156eb5ab7b4b499cd920b749ed76832
Bunny <glisa825@gmail.com>
2020-08-27 20:32:55 -0400

    Fix

--- game/modules/tome/data/maps/vaults/bandit-fortress.lua
+++ game/modules/tome/data/maps/vaults/bandit-fortress.lua
@@ -47,7 +47,7 @@ local stairs = function()
 		persistent = "zone",
 		
 		no_worldport = true,
-		min_material_level = util.bound(util.getval(game.zone.min_material_level) + 1, 1, 5) -- vaults have better loot! acts as reward to compensate for the significant difficulty
+		min_material_level = util.bound(util.getval(game.zone.min_material_level) + 1, 1, 5), -- vaults have better loot! acts as reward to compensate for the significant difficulty
 		max_material_level = util.bound(util.getval(game.zone.max_material_level) + 1, 1, 5),
 		generator = {
 			map = {
