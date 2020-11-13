3380a69e2eacd536c011938766ad2e3051e08a4a
Bunny <glisa825@gmail.com>
2020-11-12 23:04:20 -0500

    Ban fearscape on randbosses

--- game/modules/tome/data/talents/corruptions/shadowflame.lua
+++ game/modules/tome/data/talents/corruptions/shadowflame.lua
@@ -140,7 +140,7 @@ newTalent{
 	no_sustain_autoreset = true,
 	random_boss_rarity = 5,
 	rnd_boss_restrict = function(self, t, data)
-		return data.level < 20
+		return true -- allow only on fixedbosses specifically granted the talent
 	end,
 	tactical = {
 		-- heals (negative attack) demons hurts others
