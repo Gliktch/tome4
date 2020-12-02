a7c6559c478489692bc13b8495c1718c1ec3e07b
otowakotori <806682153@qq.com>
2020-12-02 22:39:27 +0800

    Fix oversized Splatter Sigils

--- game/modules/tome/data/talents/cursed/crimson-templar.lua
+++ game/modules/tome/data/talents/cursed/crimson-templar.lua
@@ -107,7 +107,7 @@ newTalent{
 														 DamageType.SOLAR_BLOOD, {dam=self:spellCrit(t.getStrength(self, t)), pow=self:combatSpellpower()},
 														 self:getTalentRadius(t),
 														 5, nil,
-														 MapEffect.new{zdepth=6, overlay_particle={zdepth=6, only_one=true, type="circle", args={appear=8, oversize=0, img="sun_sigil_dark", radius=self:getTalentRadius(t)*2}}, color_br=255, color_bg=187, color_bb=187, alpha=10, effect_shader="shader_images/sunlight_effect.png"},
+														 MapEffect.new{zdepth=6, overlay_particle={zdepth=6, only_one=true, type="circle", args={appear=8, oversize=0, img="sun_sigil_dark", radius=self:getTalentRadius(t)}}, color_br=255, color_bg=187, color_bb=187, alpha=10, effect_shader="shader_images/sunlight_effect.png"},
 														 nil, true
 														)
 	end,
