123b0aef534fbbc4e535f99e0d29c40fbd2ea1b9
yutio888 <yutio888@qq.com>
2021-07-22 10:25:29 +0800

    Living lightning correctly works with Range Amplification Device

--- game/modules/tome/data/talents/spells/energy-alchemy.lua
+++ game/modules/tome/data/talents/spells/energy-alchemy.lua
@@ -152,7 +152,7 @@ newTalent{
 	target = function(self, t) return{type="hit", range=self:getTalentRange(t), talent=t, friendlyblock=false, friendlyfire=false} end,
 	callbackOnActBase = function(self, t)
 		local tgts = {}
-		local grids = core.fov.circle_grids(self.x, self.y, 6, true)
+		local grids = core.fov.circle_grids(self.x, self.y, self:getTalentRange(t), true)
 		for x, yy in pairs(grids) do for y, _ in pairs(grids[x]) do
 			local a = game.level.map(x, y, Map.ACTOR)
 			if a and self:reactionToward(a) < 0 then
