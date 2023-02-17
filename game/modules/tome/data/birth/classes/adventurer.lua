e5b8f89de11d26c4f2f7bb46606b8e6bf056750b
Recaiden <mlwatts45@gmail.com>
2023-02-17 17:00:06 -0600

    New hook for wanderers.

--- game/modules/tome/data/birth/classes/adventurer.lua
+++ game/modules/tome/data/birth/classes/adventurer.lua
@@ -164,9 +164,7 @@ newBirthDescriptor{
 					game.bignews:say(90, "#GOLD#As you level up you learn the talent tree: #LIGHT_BLUE#%s", tostring(name))
 				end
 				self:learnTalentType(tt)
-				if tt_def.post_birth_setup then
-					tt_def.post_birth_setup(self, what, silent)
-				end
+				self:triggerHook({"Wanderer:LearntCategory", wanderer=self, tt=tt_def, silent=silent})
 			end
 		end,
 		randventurer_last_learn_level = 0,
