624a67329fe2ad440c5b344785a9c73fcf22ae63
DarkGod <darkgod@net-core.org>
2023-06-29 15:48:28 +0200

    bump version

--- game/modules/tome/init.lua
+++ game/modules/tome/init.lua
@@ -22,8 +22,8 @@ long_name = "Tales of Maj'Eyal: Age of Ascendancy"
 short_name = "tome"
 author = { "DarkGod", "darkgod@te4.org" }
 homepage = "https://te4.org/"
-version = {1,7,5}
-engine = {1,7,5,"te4"}
+version = {1,7,6}
+engine = {1,7,6,"te4"}
 description = [[
 Welcome to Maj'Eyal.
 
